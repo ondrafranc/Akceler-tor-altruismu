56a11b69af526de50fd1a1558fda7372d96ca642
Ondrej Franc <ondrafranc@seznam.cz>
2025-12-18 17:20:27 +0100

    dd

--- akcelerator-landing-page/.svelte-kit/output/server/chunks/animations.js
+++
@@ -1,8 +0,0 @@
-import { w as writable } from "./index2.js";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger.js";
-const currentLanguage = writable("czech");
-gsap.registerPlugin(ScrollTrigger);
-export {
-  currentLanguage as c
-};
